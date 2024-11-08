8b3694af01e95cee5c734e1cbe1560c51b7b7912
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-08 16:18:40 +0900

    04_090_props_2

--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -5,11 +5,15 @@
 
 import "./Child.css";
 
-const Child = ({ color: c = "green" }) => {
+const Child = ({ color: c = "green", num, fn, bool, obj }) => {
   //console.log(props);
   return (
     <div className={`component ${c}`}>
       <h3>Hello Component</h3>
+      <h3>{num}</h3>
+      <h3>{fn("Props")}</h3>
+      <h3>{bool ? "true" : "false"}</h3>
+      <h3>{obj.name + obj.age}</h3>
     </div>
   );
 };
