a89f81fa9922129eea8af2bcfa72c253e274d499
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-08 18:21:28 +0900

    04_120_props_rules

--- 04_react_basic/src/120_props_rules/start/Example.jsx
+++ 04_react_basic/src/120_props_rules/start/Example.jsx
@@ -1,14 +1,15 @@
 // POINT propsの流れは一方通行
 // POINT propsは読み取り専用
 
-import Bye from "./components/Bye"
-import Hello from "./components/Hello"
+import Bye from "./components/Bye";
+import Hello from "./components/Hello";
 
 const Example = () => {
+  const name = "Tom";
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name} />
+      <Bye name={name} />
     </>
   );
 };
