d5a8ba0a5850183aa2e16d6d148ce34dff8eb57c
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-08 13:36:50 +0900

    04_060_styling

--- 04_react_basic/src/060_styling/start/Example.jsx
+++ 04_react_basic/src/060_styling/start/Example.jsx
@@ -1,9 +1,11 @@
 /**
  * [注意]レクチャーをプルダウンで選択すると、<head>タグにそのレクチャーでimport "Example.css"のように読み込んだスタイルが挿入されます。このスタイルはプルダウンを切り替えても残りつづけるため、まだcssを記述していないのにスタイルが適用されていると感じた場合にはブラウザを更新してください。
  */
+import "./Example.css";
+
 const Example = () => {
   return (
-    <div>
+    <div className="component">
       <h3>Hello Component</h3>
     </div>
   );
