92fa2a027c1c1cab9f67ea68261c95df8f4e157c
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-11 17:25:49 +0900

    05_030_useState_render

--- 05_state_and_event/src/030_useState_render/start/Example.jsx
+++ 05_state_and_event/src/030_useState_render/start/Example.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 const Example = () => {
   let displayVal;
-  let [ val, setVal ] = useState();
-  console.log('再レンダリングされました');
+  let [val, setVal] = useState();
+  console.log("再レンダリングされました");
   return (
     <>
       <input
@@ -18,5 +18,10 @@ const Example = () => {
     </>
   );
 };
+
+// 1.Exampleコンポーネントのレンダリング
+// 2.onChange実行
+// 3.Exampleコンポーネントの再レンダリング
+// 1~3 で変更した値をどこかに保持しておく必要がある ⇒ state ⇒ これを提供しているのがuseState
 
 export default Example;
