ccb30c62a59ec90b2eb9648d3cc642e333598874
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-11 17:07:18 +0900

    05_020_useState

--- 05_state_and_event/src/020_useState/start/Example.jsx
+++ 05_state_and_event/src/020_useState/start/Example.jsx
@@ -1,8 +1,18 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  // useState 配列の0番目に参照用の値, 配列の1番目に更新用の関数
+  let [val, setVal] = useState();
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          //const setFn = valArry[1]; //更新用の関数
+          setFn(e.target.value);
+        }}
+      />
+      = {valArry[0]} //参照用の値
     </>
   );
 };
