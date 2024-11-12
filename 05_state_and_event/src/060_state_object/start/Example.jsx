d8ef1b91e9ee6bd6d1bd8f1e8ea45a364104dfc5
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-12 13:30:14 +0900

    05_060_state_object

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -2,6 +2,26 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj);
+  const changeName = (e) => {
+    // objectのセットにはobjectを渡す必要がある
+    setPerson({ name: e.target.value, age: person.age });
+  };
+  const changeAge = (e) => {
+    setPerson({ name: person.name, age: e.target.value });
+  };
+  const reset = () => {
+    setPerson({ name: "", age: "" });
+  };
+  return (
+    <>
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.age}</h3>
+      <input type="text" value={person.name} onChange={changeName}></input>
+      <input type="number" value={person.age} onChange={changeAge}></input>
+      <button onClick={reset}>リセット</button>
+    </>
+  );
 };
 
 export default Example;
