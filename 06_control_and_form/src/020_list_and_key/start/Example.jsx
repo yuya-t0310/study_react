069cba66e6e928ad2057ca180f5607f1ff882e9a
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-12 18:38:35 +0900

    06_list_and_key

--- 06_control_and_form/src/020_list_and_key/start/Example.jsx
+++ 06_control_and_form/src/020_list_and_key/start/Example.jsx
@@ -1,4 +1,3 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
@@ -21,10 +20,21 @@ const Example = () => {
         {helloAnimals} */}
         {/* {animalList} */}
 
-        {animals.map((animal) => <li>Hello, {animal}</li>)}
+        {animals.map((animal) => (
+          // リストの子要素にはユニークなkeyを設定する
+          <li key={animal}>Hello, {animal}</li>
+        ))}
       </ul>
     </>
   );
 };
+
+// Reactは要素ツリーの差分を比較してブラウザの(Real)DOMを更新(commit)する
+// keyが無い場合：Reactは全ての子React要素を変更する⇒パフォーマンス悪化
+// keyがある場合：変更のある子React要素を識別可能となり、差分のみ更新する
+
+// キーには必ず一位の値を設定する
+// キーに設定した値は変更しない。
+// 配列のインデックスはなるべく使わない。⇒配列変更時に更新時にkeyも変わる⇒更新対象が増え、パフォーマンス悪化
 
 export default Example;
