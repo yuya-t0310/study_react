d6f4dfdaf54953239b6db7697b8ae5a6a0e17ec4
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-12 18:23:06 +0900

    06_010_list_components

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,14 +1,16 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  const helloAnimals = animals.map((animal) => <li>hello, {animal}</li>);
   return (
     <>
       <h3>配列の操作</h3>
-      <ul>
-        
-      </ul>
+      {/* <ul>
+        <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li>
+      </ul> */}
+      {helloAnimals}
     </>
   );
 };
