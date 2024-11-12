22026c769dc4093211b80db59fb86322515a97c0
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-12 17:58:20 +0900

    05_090_practice_state_props

--- 05_state_and_event/src/090_practice_state_props/start/Example.jsx
+++ 05_state_and_event/src/090_practice_state_props/start/Example.jsx
@@ -1,24 +1,32 @@
+import { useState } from "react";
 
 const Example = () => {
+  const [count, setCount] = useState(0);
   return (
     <>
       <h3>練習問題</h3>
-      <p>カウントの更新（CountUpdate）と表示（CountResult）を別のコンポーネントに分離してください。Exampleコンポーネント内で現在のカウントの値を管理するstateを一つ定義してこれまでのレクチャーで実装したようなカウンターを作成してください。</p>
-      {/* このコメントアウトを外して利用！
-        <CountResult title="カウント" />
-        <CountUpdate /> 
-      */}
+      <p>
+        カウントの更新（CountUpdate）と表示（CountResult）を別のコンポーネントに分離してください。Exampleコンポーネント内で現在のカウントの値を管理するstateを一つ定義してこれまでのレクチャーで実装したようなカウンターを作成してください。
+      </p>
+      <CountResult title="カウント" count={count} />
+      <CountUpdate setCount={setCount} />
     </>
   );
 };
-const CountResult = (/* propsを定義 */) => <h3>{/* propsのtitleとcountの値を表示 */}</h3>
 
-const CountUpdate = (/* propsを定義 */) => {
+// オブジェクトリテラルを忘れずに
+const CountResult = ({ title, count }) => (
+  <h3>
+    {title}:{count}
+  </h3>
+);
+
+const CountUpdate = ({ setCount }) => {
   const countUp = () => {
-    /* countに1追加 */
+    setCount((prev) => prev + 1);
   };
   const countDown = () => {
-    /* countから1マイナス */ 
+    setCount((prev) => prev - 1);
   };
   return (
     <>
