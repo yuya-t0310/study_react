68e6e7c8bbc71809f2ef881598c67a09dd4afb63
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-12 11:36:46 +0900

    05_040_multiple_state

--- 05_state_and_event/src/040_multiple_state/start/Example.jsx
+++ 05_state_and_event/src/040_multiple_state/start/Example.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+  // useStateはコンポーネントのトップレベルでなければ呼べない
+  // if文の中などでも呼べない　⇒　トップレベルではないため
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(10);
+  const [countC, setCountC] = useState(100);
+  return (
+    <>
+      <p>ボタンAを{countA}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountA(countA + 1);
+        }}
+      >
+        countAボタン
+      </button>
+      <p>ボタンBを{countB}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountB(countB + 1);
+        }}
+      >
+        countBボタン
+      </button>
+      <p>ボタンCを{countC}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountC(countC + 1);
+        }}
+      >
+        countCボタン
+      </button>
+    </>
+  );
 };
 
 export default Example;
