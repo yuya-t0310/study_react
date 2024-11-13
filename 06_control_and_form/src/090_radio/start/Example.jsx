7e0de74008f19db377b43e046a0c6a9c16f61335
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-13 11:53:59 +0900

    06_090_radio

--- 06_control_and_form/src/090_radio/start/Example.jsx
+++ 06_control_and_form/src/090_radio/start/Example.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [fruit, setFruit] = useState("Apple");
+  const onChange = (e) => setFruit(e.target.value);
+
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map((value) => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={value}
+              checked={fruit === value}
+              onChange={onChange}
+            />
+            {value}
+          </label>
+        );
+      })}
+    </>
   );
 };
 
