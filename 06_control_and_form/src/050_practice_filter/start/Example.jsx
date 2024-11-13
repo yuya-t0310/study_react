bb1728164ad699acffe02ba893467a281101e908
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-13 10:25:12 +0900

    06_050_practice_filter

--- 06_control_and_form/src/050_practice_filter/start/Example.jsx
+++ 06_control_and_form/src/050_practice_filter/start/Example.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Profile from "./components/Profile";
 
 const persons = [
@@ -19,16 +20,33 @@ const persons = [
 ];
 
 const Example = () => {
+  const [filterVal, setFilterVal] = useState("");
   return (
     <>
       <h3>練習問題</h3>
-      <p>入力欄を設置して、入力値と名前が一致したもののみ表示する仕組みを作成してください。</p>
+      <p>
+        入力欄を設置して、入力値と名前が一致したもののみ表示する仕組みを作成してください。
+      </p>
+      <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => {
+          setFilterVal(e.target.value);
+        }}
+      />
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>
-            <Profile {...person} />
-          </li>
-        ))}
+        {persons
+          .filter((person) => person.name.indexOf(filterVal) !== -1)
+          // ↑を分解すると以下になる
+          .filter((person) => {
+            const isMatch = person.name.indexOf(filterVal);
+            return isMatch;
+          })
+          .map((person) => (
+            <li key={person.name}>
+              <Profile {...person} />
+            </li>
+          ))}
       </ul>
     </>
   );
