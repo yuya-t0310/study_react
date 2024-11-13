4be6fb04bd34bfdcf3c32569193df69dd7680d59
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-13 10:47:49 +0900

    06_060_conditional_render

--- 06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ 06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,32 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
+            console.log(animalStr.indexOf(filterVal));
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            // if (animal === "Dog") {
+            //   return <li key={animal}>{animal + "★"}</li>;
+            // } else {
+            //   <li key={animal}>{animal}</li>;
+            // }
+            // ↓ 三項演算子でかく
+            // return (
+            //   <li key={animal}>{animal === "Dog" ? animal + "★" : animal}</li>
+            // );
+            // ↓ &&条件でかく
+            return (
+              <li key={animal}>
+                {/* animalがnullまたはundefinedなら */}
+                {animal ?? "null,undefinedでした"}
+                {animal === "Dog" && "★"}
+                {/* bool値は画面に表示されない */}
+                {/* {animal + (animal === "Dog" && "★")} ではfalseが表示されてしまう*/}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
