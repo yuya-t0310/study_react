67b9cf369aa1dbc0a920cd6423df62585a491377
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2024-11-13 11:30:27 +0900

    06_080_input_textarea

--- 06_control_and_form/src/080_input_textarea/start/Example.jsx
+++ 06_control_and_form/src/080_input_textarea/start/Example.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] = useState("");
+  const clearVal = () => setVal("");
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      {/* htmlFor:ラベルをクリックしたときにフォーカスが当たるidを指定 */}
+      <label htmlFor="123">ラベル</label>
+      <div>
+        <input
+          id="123"
+          placeholder="Hello"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <textarea
+          id="456"
+          placeholder="Hello"
+          // valueはhtmlでは<textarea> value </textarea>とするが、jsxでは↓のように記述する
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>{val}</h3>
+        <button onClick={clearVal}>クリア</button>
+      </div>
+    </div>
   );
 };
 
