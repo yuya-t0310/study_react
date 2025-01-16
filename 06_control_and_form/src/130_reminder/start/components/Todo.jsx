7af76e18b0689061d3d3991cba29b501add8501a
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-01-16 15:44:03 +0900

    復習

--- 06_control_and_form/src/130_reminder/start/components/Todo.jsx
+++ 06_control_and_form/src/130_reminder/start/components/Todo.jsx
@@ -1,46 +1,42 @@
-import { useState } from "react";
-
-import List from "./List";
-import Form from "./Form";
+import { useState } from "react"
+import List from "./List"
+import Form from "./Form"
 
 const Todo = () => {
   const todosList = [
     {
-      id: 1,
-      content: "店予約する",
+      id:1,
+      content:"店予約する",
     },
     {
-      id: 2,
-      content: "卵買う",
+      id:2,
+      content:"卵買う",
     },
     {
-      id: 3,
-      content: "郵便出す",
+      id:3,
+      content:"郵便出す",
     },
   ];
 
-  const [todos, setTodos] = useState(todosList);
+  const [ todos, setTodos ] = useState(todosList);
 
-  // idを指定してtoDosListから削除
   const deleteTodo = (id) => {
     const newTodos = todos.filter((todo) => {
-      // 渡されたidに一致しないもののみをnewTodosに加える　⇒　渡されたidのみ削除される
       return todo.id !== id;
-    });
-
+    })
     setTodos(newTodos);
-  };
+  }
 
   const createTodo = (todo) => {
     setTodos([...todos, todo]);
-  };
+  }
 
   return (
     <>
-      <List todos={todos} deleteTodo={deleteTodo} />
-      <Form createTodo={createTodo} />
+      <List todos={todos} deleteTodo={deleteTodo}/>
+      <Form createTodo={createTodo}/>
     </>
-  );
+  )
 };
 
 export default Todo;
