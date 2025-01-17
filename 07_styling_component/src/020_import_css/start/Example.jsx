e2102b926534b9cc8214d961dc1a6264824f5b76
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-01-17 13:32:02 +0900

    07_020_import_css

--- 07_styling_component/src/020_import_css/start/Example.jsx
+++ 07_styling_component/src/020_import_css/start/Example.jsx
@@ -1,27 +1,19 @@
 import { useState } from "react";
 
+import "./Example.css";
+import SubButton from "./components/SubButton";
+
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
 
-  const style = {
-    margin: "auto",
-    "border-radius": "9999px",
-    border: "none",
-    display: "block",
-    width: 120,
-    height: 60,
-    fontWeight: "bold",
-    cursor: "pointer",
-    backgroundColor: isSelected ? "pink" : "",
-  };
-
   return (
     <>
-      <button style={style} onClick={clickHandler}>
+      <button className={isSelected ? "btn selected" : "btn"} onClick={clickHandler}>
         ボタン
       </button>
+      < SubButton/>
       <div style={{ textAlign: "center" }}>
         {isSelected && "クリックされました。"}
       </div>
