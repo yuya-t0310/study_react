f53b5fc5ae414ac929d2243d8dabce4a6fa3a8a6
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-10 13:38:43 +0900

    07_045_practice_css_in_js

--- 07_styling_component/src/045_practice_css_in_js/start/Example.jsx
+++ 07_styling_component/src/045_practice_css_in_js/start/Example.jsx
@@ -7,6 +7,16 @@ const FirstButton = styled.button`
   margin: 0.5rem 1rem;
   width: 11rem;
   border: none;
+  background-color: pink;
+`;
+
+const SecondButton = styled(FirstButton)`
+  background-color: red;
+  color: white;
+`;
+
+const ThirdButton = styled(SecondButton)`
+  background: ${(props) => props.dark ? "black" : "green"};
 `;
 
 const Example = () => {
@@ -28,6 +38,9 @@ const Example = () => {
         </ul>
       </p>
       <FirstButton>ボタン1</FirstButton>
+      <SecondButton>ボタン2</SecondButton>
+      <ThirdButton dark>ボタン3</ThirdButton>
+      <ThirdButton>ボタン3</ThirdButton>
     </>
   );
 };
