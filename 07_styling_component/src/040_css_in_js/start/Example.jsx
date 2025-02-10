28a9d6eeef88cd2db900e1b0bbbdca407cde975e
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-10 11:22:54 +0900

    07_040_css_in_js

--- 07_styling_component/src/040_css_in_js/start/Example.jsx
+++ 07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -1,4 +1,18 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+console.dir(styled);
+const StyledButton = styled.button`
+  margin: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${({ isSelected }) => (isSelected ? "pink" : "")};
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,6 +21,9 @@ const Example = () => {
 
   return (
     <>
+      <StyledButton $isSelected={isSelected} onClick={clickHandler}>
+        ボタン
+      </StyledButton>
       <button
         className={`btn ${isSelected ? "selected" : ""}`}
         onClick={clickHandler}
