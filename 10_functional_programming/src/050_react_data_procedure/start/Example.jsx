356e895b6a0e4da0df250350380ae75e6f216df8
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-12 13:36:25 +0900

    010_050_react_data_procedure

--- 10_functional_programming/src/050_react_data_procedure/start/Example.jsx
+++ 10_functional_programming/src/050_react_data_procedure/start/Example.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 const Example = () => {
-  const [ state, setState ] = useState(0);
+  // jsxを作成する場所
+  const [state, setState] = useState(0);
   const increment = () => {
     setState(state + 1);
-  }
+  };
+  // 副作用
+  // window.alert('hello');
   return (
     <>
       <button onClick={increment}>+</button>
