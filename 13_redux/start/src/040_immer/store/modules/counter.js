83f1adfc3be1bfa41aac555d3d3892e3c8b70e39
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-19 18:15:10 +0900

    013_040_immer

--- 13_redux/start/src/040_immer/store/modules/counter.js
+++ 13_redux/start/src/040_immer/store/modules/counter.js
@@ -2,15 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const counter = createSlice({
   name: 'counter',
-  initialState: 0,
+  initialState: {
+    count: 0
+  },
   reducers: {
     add(state, { type, payload }) {
       console.log(type, payload)
-      return state + payload;
+      // ReduxToolkit内では、mutableな操作もimmutableな操作として扱われれる（Immerライブラリによる）
+      state.count = state.count + payload;
+      // immutabilityの保持
+      // const newState = { ...state };
+      // newState.count = state.count + payload;
+      // return newState;
+
+      // ReduxToolkit内でmutableな操作を行った場合はreturnを記述しない
     },
     minus(state, { type, payload }) {
       console.log(type, payload)
-      return state - payload;
+      state.count = state.count - payload;
+      // immutabilityの保持
+      // const newState = { ...state };
+      // newState.count = state.count - payload;
+      // return newState;
     }
   }
 })
