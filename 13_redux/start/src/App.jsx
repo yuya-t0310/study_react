a9dc13886da4dfd9797715e0c073347206f0ea41
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-19 16:18:54 +0900

    013_015_multiple_reducers

--- 13_redux/start/src/App.jsx
+++ 13_redux/start/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
-import Example from "./010_redux_no_rtk/Example";
-// import Example from "./015_multiple_reducers/Example";
+// import Example from "./010_redux_no_rtk/Example";
+import Example from "./015_multiple_reducers/Example";
 // import Example from "./020_actionCreator/Example";
 // import Example from "./030_redux_toolkit/Example";
 // import Example from "./040_immer/Example";
