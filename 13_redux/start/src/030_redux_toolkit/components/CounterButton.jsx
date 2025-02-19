42c2654430424f87bda0f8b7511bb8ad518b56f3
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-19 18:05:44 +0900

    013_030_redux_toolkit

--- 13_redux/start/src/030_redux_toolkit/components/CounterButton.jsx
+++ 13_redux/start/src/030_redux_toolkit/components/CounterButton.jsx
@@ -1,16 +1,22 @@
 import { useDispatch } from "react-redux";
-import { add, minus } from "../store/modules/counter"
+import { add, minus } from "../store/modules/counter";
 
-const CounterButton = ({calcType, step}) => {
-    
-    const dispatch = useDispatch();
-    
-    const clickHandler = () => {
-        const action = calcType === '+' ? add(step) : minus(step);
-        console.log(action)
-        dispatch(action);
-    }
+const CounterButton = ({ calcType, step }) => {
+  const dispatch = useDispatch();
 
-    return <button onClick={clickHandler}>{calcType}{step}</button>
-}
+  console.log(add(step));
+
+  const clickHandler = () => {
+    const action = calcType === "+" ? add(step) : minus(step);
+    console.log(action);
+    dispatch(action);
+  };
+
+  return (
+    <button onClick={clickHandler}>
+      {calcType}
+      {step}
+    </button>
+  );
+};
 export default CounterButton;
