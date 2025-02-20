9844f98e6f6dc892ad1a8d019e06655b53f76a93
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-20 14:59:57 +0900

    015_070_useDeferredValue

--- 15_performance/src/070_useDeferredValue/start/Example.jsx
+++ 15_performance/src/070_useDeferredValue/start/Example.jsx
@@ -1,4 +1,4 @@
-import { useState, useTransition } from "react";
+import { useState, useTransition, useDeferredValue } from "react";
 
 const generateDummyItem = (num) => {
   return new Array(num).fill(null).map((item, index) => `item ${index}`);
@@ -7,29 +7,30 @@ const generateDummyItem = (num) => {
 const dummyItems = generateDummyItem(10000);
 
 const Example = () => {
-  const [isPending, startTransition] = useTransition();
+  // const [isPending, startTransition] = useTransition();
   const [filterVal, setFilterVal] = useState("");
 
   const changeHandler = (e) => {
-    startTransition(() => {
-      setFilterVal(e.target.value);
-    })
+    // startTransition(() => {
+    //   setFilterVal(e.target.value);
+    // })
+    setFilterVal(e.target.value);
   };
+
+  const filteredItems = dummyItems
+    .filter((item) => {
+      if (filterVal === "") return true;
+      return item.includes(filterVal);
+    })
+    .map((item) => <li key={item}>{item}</li>);
+
+  const deferredItems = useDeferredValue(filteredItems);
 
   return (
     <>
       <input type="text" onChange={changeHandler} />
-      {isPending && <div>Loading...</div>}
-      <ul>
-        {dummyItems
-          .filter((item) => {
-            if (filterVal === "") return true;
-            return item.includes(filterVal);
-          })
-          .map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-      </ul>
+      {/* {isPending && <div>Loading...</div>} */}
+      <ul>{deferredItems}</ul>
     </>
   );
 };
