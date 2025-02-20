abab8a949b943e3b1c7d525253b41be83df72200
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-20 17:25:30 +0900

    016_070_dynamic_import

--- 16_rest_api/src/070_dynamic_import/start/Example.jsx
+++ 16_rest_api/src/070_dynamic_import/start/Example.jsx
@@ -1,5 +1,16 @@
+import { add } from "./add"; // static import
+
 const Example = () => {
-  
+  // dynamicImport
+  const dynamicImport = async () => {
+    // 必要な時だけインポートできる ⇒ 処理が軽くなる
+    const module = await import("./add");
+    console.log(module);
+  };
+  import("./add").then((module) => {
+    console.log(module);
+  });
+  console.log(add(1, 2));
 };
 
 export default Example;
