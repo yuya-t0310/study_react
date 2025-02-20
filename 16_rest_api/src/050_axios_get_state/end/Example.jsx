0753adc743df746f765c57d562fb1d7a119314ce
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-20 16:20:04 +0900

    015_050_axios_get_state

--- 16_rest_api/src/050_axios_get_state/end/Example.jsx
+++ 16_rest_api/src/050_axios_get_state/end/Example.jsx
@@ -3,30 +3,29 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Example = () => {
-
-  const [ users, setUsers ] = useState()
+  const [users, setUsers] = useState();
 
   useEffect(() => {
     const getUser = async () => {
-      const res = await axios.get('http://localhost:3003/user')
+      const res = await axios.get("http://127.0.0.1:3003/user");
       setUsers(res.data);
-    }
+    };
     getUser();
   }, []);
 
   return (
     <div>
-      {users?.map(user => {
+      {users?.map((user) => {
         return (
           <div key={user.id}>
             <h3>{user.username}</h3>
             <p>age: {user.age}</p>
-            <p>hobby: {user.hobbies.join(',')}</p>
+            <p>hobby: {user.hobbies.join(",")}</p>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 };
 
 export default Example;
--- 16_rest_api/src/050_axios_get_state/start/Example.jsx
+++ 16_rest_api/src/050_axios_get_state/start/Example.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Example = () => {
-  return <></>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("http://127.0.0.1:3003/user");
+      setUsers(res.data);
+    };
+    getUser();
+  }, []); //第二引数を設定しないと再レンダリングされて無限ループになる
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>age: {user.age}</p>
+            <p>hobby: {user.hobbies.join(",")}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Example;
