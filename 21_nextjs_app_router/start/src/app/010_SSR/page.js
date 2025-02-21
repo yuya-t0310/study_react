3093ef8b92f4588f6e65a4f492f24d6d32c3f8e4
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-21 11:04:05 +0900

    017_002_use_server_use_client

--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,4 +1,13 @@
+import ClientComp from './components/ClientComp';
+import './lib';
 
+// サーバー側
 export default function SSR() {
-  return <div>SSR Page</div>
+
+  return (
+    <>
+    <div>SSR Page</div>
+    <ClientComp />
+    </>    
+  )
 }
