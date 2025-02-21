68f01e56bf04e59e201de27d3ffc0d71ed5fa2b9
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-21 11:20:17 +0900

    017_003_use_API

--- 21_nextjs_app_router/start/src/app/010_SSR/layout.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/layout.js
@@ -1,6 +1,6 @@
 export default function SubLayout({children}){
   return (
-    <div style={{background:'red'}}>
+    <div style={{background:'#c1e4e9'}}>
     {children}
     </div>
   )
--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,13 +1,25 @@
+import { ENDPOINT } from '@/constants';
 import ClientComp from './components/ClientComp';
 import './lib';
+import ArticleList from '@/components/articleList';
 
 // サーバー側
-export default function SSR() {
+export default async function SSR() {
+
+  // 副作用であるので通常はuseEffect内に記述しなければならないが、サーバーコンポーネントでは直接記述してよい
+  const articles = await fetch(ENDPOINT, {
+    cache: 'no-store', // 毎回キャッシュを使わない force-cacheはその逆
+    next : {revalidate:10} // 10秒以内であればキャッシュを利用
+  }).then(res => res.json());
+
+  //next.jsのfetch関数
+  //第二引数にオプションを設定できる
 
   return (
     <>
     <div>SSR Page</div>
     <ClientComp />
+    <ArticleList list={articles} basePath="/010_SSR" />
     </>    
   )
 }
