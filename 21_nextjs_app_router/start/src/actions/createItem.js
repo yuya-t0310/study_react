1bae3023830ccaf764cc21dca3d3ecbd2a03a83e
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-27 18:18:19 +0900

    017_008_server_action

--- 21_nextjs_app_router/start/src/actions/createItem.js
+++ 21_nextjs_app_router/start/src/actions/createItem.js
@@ -1,6 +1,31 @@
-
+"use server";
 import { ENDPOINT } from "@/constants";
 
-export async function createItem(formData) {
-  
+export async function createItem(state, formData) {
+  console.log(state.msg);
+  const id = formData.get("id");
+  const title = formData.get("title");
+  console.log(id, title);
+
+  if (id === "" || title === "") {
+    return { msg: "入力フィールドが空です。" };
+  }
+
+  try {
+    // catchのテスト
+    //throw "error";
+    // JSONサーバにPOSTメソッドを投げる
+    const res = await fetch(ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, title }),
+    });
+    // JSONサーバから返ってきたJSONをレスポンスとしてAPIの戻り値にする
+    const data = await res.json();
+    return { msg: `${data.id}:${data.title}の登録が完了しました。` };
+  } catch {
+    return { msg: "登録に失敗しました。" };
+  }
 }
--- 21_nextjs_app_router/start/src/app/050_actions/components/form.js
+++ 21_nextjs_app_router/start/src/app/050_actions/components/form.js
@@ -1,30 +1,42 @@
-'use client';
+"use client";
 
 import { createItem } from "@/actions/createItem";
 import { useState } from "react";
+import { useFormState } from "react-dom";
 
 export default function ArticleForm() {
   const [newId, setNewId] = useState();
   const [newTitle, setNewTitle] = useState();
 
+  const [state, createItemAction] = useFormState(createItem, { msg: null });
   return (
-    <form>
+    // サーバーアクションを使う際はmethodを指定できない
+    <form action={createItemAction}>
       <div>
         <label>
           {" "}
           Id:
-          <input type="number" name="id" value={newId} onChange={(e) => setNewId(e.target.value)} />
+          <input
+            type="number"
+            name="id"
+            value={newId}
+            onChange={(e) => setNewId(e.target.value)}
+          />
         </label>
       </div>
       <div>
         <label>
           {" "}
           Title:
-          <input name="title" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+          <input
+            name="title"
+            value={newTitle}
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
         </label>
       </div>
       <button type="submit">送信</button>
-      <div style={{ color: 'red' }}></div>
+      <div style={{ color: "red" }}>{state.msg}</div>
     </form>
   );
 }
