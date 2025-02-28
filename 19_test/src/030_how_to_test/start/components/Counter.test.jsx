2fe5849b3614ea52dd5c73ad71d17ed5c0b99cdb
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-02-28 10:02:16 +0900

    020_030_how_to_test

--- 19_test/src/030_how_to_test/start/components/Counter.test.jsx
+++ 19_test/src/030_how_to_test/start/components/Counter.test.jsx
@@ -1,6 +1,24 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
+
 /* POINT テストコードを書く際の注意点
 Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+test("カウントアップボタンで現在のカウントが+1されるか", () => {
+  // Arrage
+  render(<Counter originCount={0} />);
+
+  // 更新前
+  const spanElBeforeUpdate = screen.getByText("現在のカウント:0");
+  expect(spanElBeforeUpdate).toBeInTheDocument();
+
+  // Act
+  const btn = screen.getByRole("button", { name: "カウントアップ" });
+  fireEvent.click(btn);
+
+  // Assertion
+  const spanEl = screen.getByText("現在のカウント:1");
+  expect(spanEl).toBeInTheDocument();
+});
