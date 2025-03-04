c185fa65edf692eb1e8e641dd67ce0efe13a0cad
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-03-04 18:56:28 +0900

    003_100_async_await

--- 03_js_basic/100_async_await/start/main.js
+++ 03_js_basic/100_async_await/start/main.js
@@ -1,17 +1,32 @@
 // 非同期処理（Promise）
+// let a = 0;
+
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         a = 1;
+//         resolve(a)
+//     }, 2000);
+// }).then((b) => {
+//     console.log(b);
+//     return b;
+// }).then((b) => {
+//     console.log(b);
+// }).catch((c) => {
+//     console.log('catchが実行', c)
+// })
+
+// await/asyncで記載
 let a = 0;
-
-new Promise((resolve, reject) => {
-    setTimeout(() => {
+async function init() {
+  try {
+    const result = await new Promise((resolve, reject) => {
+      setTimeout(() => {
         a = 1;
-        resolve(a)
-    }, 2000);
-}).then((b) => {
-    console.log(b);
-    return b;
-}).then((b) => {
-    console.log(b);
-}).catch((c) => {
-    console.log('catchが実行', c)
-})
-
+        resolve(a);
+      }, 2000);
+    });
+    console.log(result);
+  } catch (e) {
+    console.log("catchが実行", e);
+  }
+}
