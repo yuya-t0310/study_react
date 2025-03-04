19c8a0e76d60060904dae8fb60c2bfde70637687
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-03-04 13:47:21 +0900

    021_080_function_type

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,15 @@
 const Example = () => {
   // 練習用
+  // 引数は型定義が必須
+  function sum1(x: number, y: number){
+    return x + y;
+  }
+
+  console.log(sum1(1, 2))
+
+  // ?をつけると任意の引数となる
+  // ただし型定義にundefinedが追加される
+  const sum2 = (x: number, y?: number) => x + y;
 };
 
 export default Example;
