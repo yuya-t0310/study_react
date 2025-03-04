fbed24551f2539e11ff8d52afd7ee00d1cb9202e
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-03-04 14:41:37 +0900

    021_120_props

--- 20_typescript/src/120_props/start/Example.tsx
+++ 20_typescript/src/120_props/start/Example.tsx
@@ -1,5 +1,14 @@
-const Example = () => {
-  // 練習用
+import Hello, { Btn } from "./Hello";
+
+const Example: React.FC = () => {
+  // 練習
+  return (
+    <>
+      <Btn fn={(text) => console.log(`Hello ${text}`)} />
+      <Hello text="TypeScript">Children</Hello>
+      <Hello />
+    </>
+  );
 };
 
 export default Example;
