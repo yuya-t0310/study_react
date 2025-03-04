5737c6841735ed252d4707977d281ec43e84fee4
tanaka.yuy <tanaka.yuy@saxa.co.jp>
2025-03-04 17:15:24 +0900

    003_040_callback

--- 03_js_basic/040_callback/start/main.js
+++ 03_js_basic/040_callback/start/main.js
@@ -1,15 +1,24 @@
+// コールバック関数：引数に他の関数が渡ってくる
+function print(callback) {
+  console.log(callback);
+  const result = callback(2);
+  console.log(result);
+}
+
 function fn(number) {
-    return number * 2;
-  }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+  return number * 2;
+}
+
+print(fn);
+
+console.log(fn(2));
+console.log(fn(4));
+
+const fnArrow = (number) => {
+  console.log(number);
+  return number * 2;
+};
+
+const fnArrowObj = (number) => ({ result: number * 2 });
+
+console.log(fnArrowObj(2));
